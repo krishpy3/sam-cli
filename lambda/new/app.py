143259601416cea782dff6bdb93df8bce4cc0d6b
Krish <krishpy3@gmail.com>
2024-03-20 13:17:07 +0530

    test4

--- lambda/new/app.py
+++ lambda/new/app.py
@@ -2,5 +2,5 @@ def lambda_handler(event, context):
     print("Hello from Lambda!")
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda v3!'
+        'body': 'Hello from Lambda v4!'
     }
